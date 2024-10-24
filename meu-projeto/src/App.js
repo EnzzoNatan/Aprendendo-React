548092113877c602bfc290723d0da4ff4b567607
EnzzoNatan <enzzoferrazz1909@gmail.com>
2024-10-24 12:06:07 -0300

    Primeiros passos com react

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const name = 'Joaquim';
+  
+
+ return(
+  <div className="App">
+    <h1>Alterando o Jsx</h1>
+    <p>Ola, {name}</p>
+
+
+  </div>
+
+ )
+
+
+}
+
+export default App;
