506eb5268bc8820a3c83eca9ec6f8fea68416d48
EnzzoNatan <enzzoferrazz1909@gmail.com>
2024-10-24 12:15:46 -0300

    Alterando o JSX

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -5,12 +5,14 @@ function App() {
 
   const name = 'Joaquim';
   
+  const url = 'https://via.placeholder.com/150';
 
  return(
   <div className="App">
     <h1>Alterando o Jsx</h1>
     <p>Ola, {name}</p>
 
+    <img src={url} alt="Imagem aleatoria"/>
 
   </div>
 
